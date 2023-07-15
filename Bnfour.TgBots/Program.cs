61b4a4b82db482b7a837cd44aeacccc867b43d01
Bn4 <bnfour@bnfour.net>
2023-07-15 23:22:55 +0700

    added serving of images

--- Bnfour.TgBots/Program.cs
+++ Bnfour.TgBots/Program.cs
@@ -2,5 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "\"Hello World!\" from bnfour's Telegram bots, now on dotnet!");
+app.UseStaticFiles(new StaticFileOptions
+{
+    // only images are currently expected to being served as static files
+    RequestPath = "/i",
+});
 
 app.Run();
