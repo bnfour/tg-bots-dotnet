33af209bee37ab7f4c0fb9faf982a03f69c62083
Bn4 <bnfour@bnfour.net>
2023-07-17 21:29:11 +0700

    bot manager plugged into DI, lifecycle hooks for setting and removing webhooks

--- Bnfour.TgBots/Program.cs
+++ Bnfour.TgBots/Program.cs
@@ -1,7 +1,11 @@
+using Bnfour.TgBots.Interfaces;
 using Bnfour.TgBots.Options;
+using Bnfour.TgBots.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<IBotManagerService, BotManagerService>();
 
 builder.Services.Configure<ApplicationOptions>(builder.Configuration.GetSection("Options"));
 
@@ -13,5 +17,11 @@ app.MapControllers();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}");
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+    await (app.Services.GetService(typeof(IBotManagerService)) as IBotManagerService)!.SetWebhooks());
+
+app.Lifetime.ApplicationStopped.Register(async () =>
+    await (app.Services.GetService(typeof(IBotManagerService)) as IBotManagerService)!.RemoveWebhooks());
 
 app.Run();
