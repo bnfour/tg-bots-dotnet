ae89a93d053a3b65515bc7b30798fe3373c2f9cd
Bn4 <bnfour@bnfour.net>
2023-07-17 21:10:50 +0700

    xmldocs for previous commit

--- Bnfour.TgBots/Bots/BotBase.cs
+++ Bnfour.TgBots/Bots/BotBase.cs
@@ -1,4 +1,3 @@
-using System;
 using Bnfour.TgBots.Options.BotOptions;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -11,16 +10,35 @@ namespace Bnfour.TgBots.Bots;
 /// </summary>
 public abstract class BotBase
 {
+    /// <summary>
+    /// Telegram bot client instance to use.
+    /// </summary>
     protected readonly TelegramBotClient? _client;
 
+    /// <summary>
+    /// URL to be used as webhook endpoint. If null, the bot is considered disabled.
+    /// </summary>
     private readonly string? _webhookUrl;
 
+    /// <summary>
+    /// Indicates if the bot is inline and is able to handle inline queries.
+    /// </summary>
     protected abstract bool Inline { get; }
 
+    /// <summary>
+    /// If set to false, the bot is not available and will throw on API usage.
+    /// Checking for this is supposed to be caller's responsibility.
+    /// </summary>
     public bool Enabled => _client != null && _webhookUrl != null;
 
     // TODO looks scuffed, any way to include the index URL in the options
     // while still declaring it once?
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="webhookIndex">Common part of the webhook endpoint path, shared between bots.</param>
+    /// <param name="options">Bot-specific options.</param>
     public BotBase(string webhookIndex, BotOptionsBase options)
     {
         if (options.Token != null)
@@ -31,6 +49,9 @@ public abstract class BotBase
         }
     }
 
+    /// <summary>
+    /// Checks if the configuration is provided. If not, throws.
+    /// </summary>
     private void ThrowIfNotEnabled()
     {
         if (!Enabled)
@@ -41,6 +62,9 @@ public abstract class BotBase
 
     #region webhook manipulation
 
+    /// <summary>
+    /// Sets the webhook.
+    /// </summary>
     public async Task SetWebhook()
     {
         ThrowIfNotEnabled();
@@ -55,6 +79,9 @@ public abstract class BotBase
         
     }
 
+    /// <summary>
+    /// Removes the webhook.
+    /// </summary>
     public async Task RemoveWebhook()
     {
         ThrowIfNotEnabled();
@@ -64,6 +91,11 @@ public abstract class BotBase
 
     #endregion
 
+    /// <summary>
+    /// Sends a message, formated in MarkdownV2. See https://core.telegram.org/bots/api#markdownv2-style
+    /// </summary>
+    /// <param name="accountId">ID of the user to send message to.</param>
+    /// <param name="message">Message to send, in MarkdownV2.</param>
     protected async Task Send(long accountId, string message)
     {
         ThrowIfNotEnabled();
@@ -74,6 +106,10 @@ public abstract class BotBase
             parseMode: ParseMode.MarkdownV2, disableWebPagePreview: true);
     }
 
+    /// <summary>
+    /// Public entry point for handling messages from Telegram backend.
+    /// </summary>
+    /// <param name="update">Update to process.</param>
     public async Task HandleUpdate(Update update)
     {
         ThrowIfNotEnabled();
@@ -100,6 +136,10 @@ public abstract class BotBase
         }
     }
 
+    /// <summary>
+    /// Handles a Message, which is anything sent to bot directly via chat: text, image, sticker etc.
+    /// </summary>
+    /// <param name="message">Message to process.</param>
     protected async Task HandleMessage(Message message)
     {
         if (message.From == null)
@@ -119,8 +159,16 @@ public abstract class BotBase
         }
     }
 
+    /// <summary>
+    /// Handles an inline query, if the bot is designated as an inline bot.
+    /// </summary>
+    /// <param name="inlineQuery">Inline query to process.</param>
     protected abstract Task HandleInlineQuery(InlineQuery inlineQuery);
 
+    /// <summary>
+    /// Handles text message.
+    /// </summary>
+    /// <param name="message">Message to process. Should be of <see cref="Message.Text"/> type and contain text.</param>
     protected async Task HandleText(Message message)
     {
         var text = message.Text ?? String.Empty;
@@ -136,9 +184,15 @@ public abstract class BotBase
         }
     }
 
+    /// <summary>
+    /// Handles commands (actually, text starting with "/") sent by users.
+    /// If a matching command is found, it is run, otherwise an error message is sent.
+    /// </summary>
+    /// <param name="userId">User that sent the command.</param>
+    /// <param name="text">Full text of the command.</param>
     protected async Task HandleCommand(long userId, string text)
     {
-        var command = text.Split(' ', '\n').First().ToLowerInvariant();
+        var command = text.Trim().Split(' ', '\n').First().ToLowerInvariant();
 
         var commandHandled = await TryToFindAndRunCommand(command, userId, text);
 
@@ -148,6 +202,13 @@ public abstract class BotBase
         }
     }
 
+    /// <summary>
+    /// Tries to find and run a command by its name.
+    /// </summary>
+    /// <param name="command">Actual command, text from starting slash up to the first whitespace.</param>
+    /// <param name="userId">User that sent the command.</param>
+    /// <param name="fullText">Full text of the command, in case arguments matter.</param>
+    /// <returns>True if command was found and executed, false otherwise.</returns>
     protected async Task<bool> TryToFindAndRunCommand(string command, long userId, string fullText)
     {
         switch (command)
@@ -167,15 +228,24 @@ public abstract class BotBase
     }
 
     // note to self: don't forget the replies are in MarkdownV2
-    // TODO make these strings changeable in descendants
+    // TODO make these strings changeable in descendants, finalize the wordings
+    // maybe some variables for bot instances to fill, like its name?
 
     #region error replies
 
+    /// <summary>
+    /// Sends a message indicating the text (or other unsupported message type) was not recognized (it's not a command at all).
+    /// </summary>
+    /// <param name="userId">ID of the user to send the message to.</param>
     protected async Task ReplyToArbitaryText(long userId)
     {
         await Send(userId, @"I did not quite catch that\.");
     }
 
+    /// <summary>
+    /// Sends a message indicating the text was recognized as a command, but there is no such command available.
+    /// </summary>
+    /// <param name="userId">ID of the user to send the message to.</param>
     protected async Task ReplyToUnknownCommand(long userId)
     {
         await Send(userId, @"Unfortunately, this is not a command I recognize\. Try running /help command\.");
@@ -185,16 +255,28 @@ public abstract class BotBase
 
     #region commands
 
+    /// <summary>
+    /// Handles /start command by sending a welcome message to the user.
+    /// </summary>
+    /// <param name="userId">ID of the user to send the message to.</param>
     protected async Task HandleStartCommand(long userId)
     {
         await Send(userId, @"Welcome\!");
     }
 
+    /// <summary>
+    /// Handles /help command by sending a usage help message to the user.
+    /// </summary>
+    /// <param name="userId">ID of the user to send the message to.</param>
     protected async Task HandleHelpCommand(long userId)
     {
         await Send(userId, @"Some kind of message about how to use this bot I guess\.");
     }
 
+    /// <summary>
+    /// Handles /about command by sending bot info: name, version, and repo link.
+    /// </summary>
+    /// <param name="userId">ID of the user to send the message to.</param>
     protected async Task HandleAboutCommand(long userId)
     {
         await Send(userId, """
