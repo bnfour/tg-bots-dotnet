617c7c10508e939a84fcfdfd6e622d176ee88d24
Bn4 <bnfour@bnfour.net>
2023-07-17 21:51:32 +0700

    first implementation of bot manager
    no bots, no proper error indication yet

--- Bnfour.TgBots/Services/BotManagerService.cs
+++ Bnfour.TgBots/Services/BotManagerService.cs
@@ -1,22 +1,59 @@
+using Bnfour.TgBots.Bots;
 using Bnfour.TgBots.Interfaces;
+using Bnfour.TgBots.Options;
+using Microsoft.Extensions.Options;
 using Telegram.Bot.Types;
 
 namespace Bnfour.TgBots.Services;
 
 public class BotManagerService : IBotManagerService
 {
-    public Task HandleUpdate(string token, Update update)
+    /// <summary>
+    /// List of bots available in this app.
+    /// </summary>
+    private readonly List<BotBase> _bots;
+
+    /// <summary>
+    /// Shorthand property to query only enabled bots.
+    /// </summary>
+    private IEnumerable<BotBase> _activeBots => _bots.Where(b => b.Enabled);
+
+    public BotManagerService(IOptions<ApplicationOptions> options)
     {
-        throw new NotImplementedException();
+        // TODO fill with bot instances
+        _bots = new()
+        {
+            // create and configure bots here
+        };
     }
 
-    public Task RemoveWebhooks()
+    public async Task HandleUpdate(string token, Update update)
     {
-        throw new NotImplementedException();
+        var bot = _activeBots.SingleOrDefault(b => b.IsToken(token));
+
+        if (bot != null)
+        {
+            await bot.HandleUpdate(update);
+        }
+        else
+        {
+            // TODO throw something to return 404 status code
+        }
     }
 
-    public Task SetWebhooks()
+    public async Task SetWebhooks()
     {
-        throw new NotImplementedException();
+        foreach (var bot in _activeBots)
+        {
+            await bot.SetWebhook();
+        }
+    }
+
+    public async Task RemoveWebhooks()
+    {
+        foreach (var bot in _activeBots)
+        {
+            await bot.RemoveWebhook();
+        }
     }
 }
