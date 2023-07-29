01ff2fecd72655f83f4e38fce8a21604450fbe37
Bn4 <bnfour@bnfour.net>
2023-07-29 23:01:23 +0700

    updated comments to include newly returned status codes

--- Bnfour.TgBots/Controllers/TelegramApiController.cs
+++ Bnfour.TgBots/Controllers/TelegramApiController.cs
@@ -23,7 +23,8 @@ public class TelegramApiController : Controller
     /// <param name="token">Token of the bot the call is addressed to.
     /// Also used to verify it's actually Telegram calling us, as no one else is supposed to know it.</param>
     /// <param name="update">Update with a message from a user we want to process.</param>
-    /// <returns>HTTP status code:<br/>200 if request is processed,<br/>404 if no bot with given token is found,<br/>500 if anything goes wrong.</returns>
+    /// <returns>HTTP status code:<br/>- 200 if request is processed,<br/>- 404 if no bot with given token is found,<br/>
+    /// - 400 if request is malformed,<br/>- 422 if request contains an inline query to a not-inline bot<br/>- 500 if anything else goes wrong.</returns>
     [HttpPost, Route("{token}")]
     public async Task<ActionResult> HandleTelegramCall(string token, [FromBody] Update update)
     {
