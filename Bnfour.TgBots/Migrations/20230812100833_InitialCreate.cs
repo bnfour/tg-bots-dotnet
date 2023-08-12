227a6e0442d92670dd54ea86144d3897ce6859cc
Bn4 <bnfour@bnfour.net>
2023-08-12 17:11:20 +0700

    initial database migration for cat macro bot that covers the config set in last commit

---
+++ Bnfour.TgBots/Migrations/20230812100833_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bnfour.TgBots.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CatMacros",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Caption = table.Column<string>(type: "TEXT", nullable: false),
+                    MediaId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatMacros", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatMacros_Caption",
+                table: "CatMacros",
+                column: "Caption",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatMacros_MediaId",
+                table: "CatMacros",
+                column: "MediaId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CatMacros");
+        }
+    }
+}
