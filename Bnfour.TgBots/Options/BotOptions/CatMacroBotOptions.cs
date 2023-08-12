5624ed8dcaada1e1148c2e49f063e904168bf6c9
Bn4 <bnfour@bnfour.net>
2023-08-12 16:32:33 +0700

    added options for cat macro bot
    subject to change

---
+++ Bnfour.TgBots/Options/BotOptions/CatMacroBotOptions.cs
@@ -0,0 +1,16 @@
+namespace Bnfour.TgBots.Options.BotOptions
+{
+    public class CatMacroBotOptions : BotOptionsBase
+    {
+        /// <summary>
+        /// List of user IDs that are considered the bot's admins,
+        /// and are able to manage the database.
+        /// </summary>
+        public List<long>? Admins { get; set; }
+
+        // please note that this bot configuration also includes
+        // a connection string to its own little database
+        // it's defined in "ConnectionStrings" section in appsettings.json
+        // as "CatMacroBotConnectionString"
+    }
+}
