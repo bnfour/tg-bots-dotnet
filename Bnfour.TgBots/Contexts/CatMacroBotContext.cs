363925cc0f3783916624f2c2966c0c706bcae66f
Bn4 <bnfour@bnfour.net>
2023-08-12 17:35:23 +0700

    new namespace syntax for CatMacroBotContext

--- Bnfour.TgBots/Contexts/CatMacroBotContext.cs
+++ Bnfour.TgBots/Contexts/CatMacroBotContext.cs
@@ -1,38 +1,38 @@
 using Bnfour.TgBots.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Bnfour.TgBots.Contexts
+namespace Bnfour.TgBots.Contexts;
+
+/// <summary>
+/// Database context for the cat macro bot.
+/// </summary>
+public class CatMacroBotContext : DbContext
 {
     /// <summary>
-    /// Database context for the cat macro bot.
+    /// The database set.
     /// </summary>
-    public class CatMacroBotContext : DbContext
+    public required DbSet<CatMacro> Images;
+
+    public CatMacroBotContext(DbContextOptions<CatMacroBotContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        /// <summary>
-        /// The database set.
-        /// </summary>
-        public required DbSet<CatMacro> Images;
+        // set table name and PK
+        // EF is nice enough to set up guid generation for us
+        modelBuilder.Entity<CatMacro>()
+            .ToTable("CatMacros")
+            .HasKey(cm => cm.Id);
 
-        public CatMacroBotContext(DbContextOptions<CatMacroBotContext> options) : base(options) { }
+        // set other field as required and unique
+        modelBuilder.Entity<CatMacro>()
+            .Property(cm => cm.Caption).IsRequired();
+        modelBuilder.Entity<CatMacro>()
+            .HasIndex(cm => cm.Caption).IsUnique();
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // set table name and PK
-            // EF is nice enough to set up guid generation for us
-            modelBuilder.Entity<CatMacro>()
-                .ToTable("CatMacros")
-                .HasKey(cm => cm.Id);
-
-            // set other field as required and unique
-            modelBuilder.Entity<CatMacro>()
-                .Property(cm => cm.Caption).IsRequired();
-            modelBuilder.Entity<CatMacro>()
-                .HasIndex(cm => cm.Caption).IsUnique();
-
-            modelBuilder.Entity<CatMacro>()
-                .Property(cm => cm.MediaId).IsRequired();
-            modelBuilder.Entity<CatMacro>()
-                .HasIndex(cm => cm.MediaId).IsUnique();
-        }
+        modelBuilder.Entity<CatMacro>()
+            .Property(cm => cm.MediaId).IsRequired();
+        modelBuilder.Entity<CatMacro>()
+            .HasIndex(cm => cm.MediaId).IsUnique();
     }
 }
+
