6d71b45dc98f8f0fe3c09f443f0e253c81458ecd
Bn4 <bnfour@bnfour.net>
2023-08-12 17:37:17 +0700

    new namespace syntax for CatMacro which i forgot when updating the database branch the first time

--- Bnfour.TgBots/Entities/CatMacro.cs
+++ Bnfour.TgBots/Entities/CatMacro.cs
@@ -1,23 +1,22 @@
-namespace Bnfour.TgBots.Entities
+namespace Bnfour.TgBots.Entities;
+
+/// <summary>
+/// Represents a saved image with a caption.
+/// </summary>
+public class CatMacro
 {
     /// <summary>
-    /// Represents a saved image with a caption.
+    /// Primary key.
     /// </summary>
-    public class CatMacro
-    {
-        /// <summary>
-        /// Primary key.
-        /// </summary>
-        public required Guid Id { get; set; }
+    public required Guid Id { get; set; }
 
-        /// <summary>
-        /// Caption, under which the image is searchable.
-        /// </summary>
-        public required string Caption { get; set; }
+    /// <summary>
+    /// Caption, under which the image is searchable.
+    /// </summary>
+    public required string Caption { get; set; }
 
-        /// <summary>
-        /// Media ID of the image.
-        /// </summary>
-        public required string MediaId { get; set; }
-    }
+    /// <summary>
+    /// Media ID of the image.
+    /// </summary>
+    public required string MediaId { get; set; }
 }
