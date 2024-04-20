d23faf84f6f7e006609efe5c416abf409f0cd0cf
Bn4 <bnfour@bnfour.net>
2024-04-21 00:29:33 +0700

    re-implemented service to get bot info for the web, not tested yet -- test will be commenced after everything's done
    the "primary constructor" thing looks funny, but let's try it anyway

--- Bnfour.TgBots/Services/BotInfoProviderService.cs
+++ Bnfour.TgBots/Services/BotInfoProviderService.cs
@@ -1,13 +1,21 @@
+using Bnfour.TgBots.Interfaces.Factories;
 using Bnfour.TgBots.Interfaces.Services;
 using Bnfour.TgBots.Models;
 
 namespace Bnfour.TgBots.Services;
 
-public class BotInfoProviderService : IBotInfoProviderService
+public class BotInfoProviderService(IBotInfoFactory factory) : IBotInfoProviderService
 {
+    private readonly IBotInfoFactory _factory = factory;
+
     public async Task<IEnumerable<BotInfoModel>> GetInfo()
     {
-        // TODO instantiate _all_ bots (even inactive) to get their basic info
-        throw new NotImplementedException();
+        var ret = new List<BotInfoModel>();
+        foreach (var bot in _factory.GetBotInfo())
+        {
+            var info = await bot.GetModel();
+            ret.Add(info);
+        }
+        return ret;
     }
 }
